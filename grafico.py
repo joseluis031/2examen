0ee74fdfc6e85bc7e57e1e9688c46d783a6366ca
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-04-28 10:21:50 +0200

    consigo una grafica pero no la que busco

---
+++ grafico.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from math import sqrt
+
+
+
+class Grafico:
+    def __init__(self,dataset,col1,col2):
+        self.dataset = dataset
+        self.col1 = col1
+        self.col2 = col2
+        
+    def grafico(self,title):
+        self.dataset = pd.read_csv("entrenador 1.csv")
+        self.dataset = pd.read_csv("entrenador 1.csv")
+        self.dataset.groupby(self.col1)[self.col2].sum().plot(kind="bar")
+        plt.title(title)
+        plt.xlabel(None)
+        plt.show()
+        
+grafico2 = Grafico("Pokemon.csv","Attack","Defense")
+grafico2.grafico("Grafico Ataque-Defensa")
