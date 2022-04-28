5de5d1dbaf80eaf460031f3ff77b1e25353fc9e9
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-04-28 09:19:29 +0200

    calculo la desviacion

--- Clases/ejercicio.py
+++ Clases/ejercicio.py
@@ -5,6 +5,12 @@ from math import sqrt
 class Ejercicio:
     def __init__(self,datos):
         self.datos = pd.read_csv(datos)
-        
+        self.N = self.datos["#"].sum()
+
     def calculo_media(self):
-        
+        suma = self.datos["Total"].sum()
+        return suma/self.N
+    
+    def calculo_desvi(self):
+        suma_desvi = self.datos["Total"].sum() * self.datos[("#"- self.calculo_media)**2]
+        return sqrt(suma_desvi/self.N)
