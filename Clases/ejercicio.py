8219487b90c8dfd079c5523446bc8c3ac03093de
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-04-28 10:13:04 +0200

    funcion porcent

--- Clases/ejercicio.py
+++ Clases/ejercicio.py
@@ -30,6 +30,32 @@ class Ejercicio:
         suma_desvi = self.datos["Total"].sum() * ((self.N- self.calculo_media)**2)
         return sqrt(suma_desvi/self.N)
 
-ejercicio = Ejercicio("entrenador 1.csv")
-print("La media es: " ,ejercicio.calculo_media())
-print(ejercicio.calculo_desvi())
+    def calculo_porcent_68(self):
+        intervalo = []
+        inferior = self.calculo_media() - self.calculo_desvi()
+        superior = self.calculo_media() + self.calculo_desvi()
+        for i in range(400):
+            if self.datos["#"][i]>inferior and self.datos["#"][i]<superior:
+                intervalo.append(self.datos[self.N][i])
+        suma_intervalo = sum(intervalo)
+        return print("El porcentaje es {}%".format(suma_intervalo/self.N))
+    
+    def calculo_porcent_95(self):
+        intervalo = []
+        inferior = self.calculo_media() - 2*self.calculo_desvi()
+        superior = self.calculo_media() + 2*self.calculo_desvi()
+        for i in range(400):
+            if self.datos["#"][i]>inferior and self.datos["#"][i]<superior:
+                intervalo.append(self.datos[self.N][i])
+        suma_intervalo = sum(intervalo)
+        return print("El porcentaje es {}%".format(suma_intervalo/self.N))
+    
+    def calculo_porcent_99_7(self):
+        intervalo = []
+        inferior = self.calculo_media() - 3*self.calculo_desvi()
+        superior = self.calculo_media() + 3*self.calculo_desvi()
+        for i in range(400):
+            if self.datos["#"][i]>inferior and self.datos["#"][i]<superior:
+                intervalo.append(self.datos[self.N][i])
+        suma_intervalo = sum(intervalo)
+        return print("El porcentaje es {}%".format(suma_intervalo/self.N))
