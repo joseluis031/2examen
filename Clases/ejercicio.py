4b8deba8cb41455ed558d0b7d3414aa1e3f4f3de
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-04-28 09:28:14 +0200

    mas medias

--- Clases/ejercicio.py
+++ Clases/ejercicio.py
@@ -11,6 +11,21 @@ class Ejercicio:
         suma = self.datos["Total"].sum()
         return suma/self.N
     
+    def calculo_media_ataque(self):
+        suma = self.datos["Attack"].sum()
+        return suma/self.N
+    
+    def calculo_media_defense(self):
+        suma = self.datos["Defense"].sum()
+        return suma/self.N
+    
+    def calculo_media_hp(self):
+        suma = self.datos["HP"].sum()
+        return suma/self.N
+    
+    
+    
+    
     def calculo_desvi(self):
         suma_desvi = self.datos["Total"].sum() * self.datos[("#"- self.calculo_media)**2]
         return sqrt(suma_desvi/self.N)
